f56def68aa32149b08d6e4bc5ff4bb2dacad3f3a
DelWise <turshatov@gmail.com>
2018-03-05 17:51:48 +0200

    ready movement and reproduction

---
+++ SimpleGame/Assets/LifeCycle.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+public class LifeCycle : MonoBehaviour
+{
+    public float lifeTime = 20;
+    public float matureAfter = 10;
+    public float reproductionPause = 2;
+
+    public Material young;
+    public Material ready;
+    public Material notReady;
+
+    public GameObject individual;
+
+    float deathTime;
+    float matureTime;
+    float nextReproduction;
+
+    bool isMature = false;
+
+    MeshRenderer renderer;
+
+    public bool readyToReproduce = false;
+    public bool canOnlyMove;
+
+    private void Start()
+    {
+        transform.localScale = Vector3.one * 0.1f;
+        renderer = GetComponent<MeshRenderer>();
+        renderer.material = young;
+        deathTime = Time.time + lifeTime;
+        matureTime = Time.time + matureAfter;
+    }
+
+    private void Update()
+    {
+
+        if (Time.time > deathTime)
+            Destroy(gameObject);
+
+        if (!isMature)
+        {
+            if (transform.localScale.x <= 1)
+                transform.localScale *= (1 + Time.deltaTime);
+            else
+                transform.localScale = Vector3.one;
+
+            if (Time.time > matureTime)
+            {
+                renderer.material = ready;
+                isMature = true;
+                readyToReproduce = true;
+            }
+        }
+        else
+        {
+            if (canOnlyMove)
+                return;
+
+            if (readyToReproduce)
+            {
+                if (Time.time > nextReproduction + 2)
+                {
+                    if (DetectPartner())
+                    {
+                        GiveBirth();
+                        readyToReproduce = false;
+                        renderer.material = notReady;
+                    }
+                }
+            }
+            else
+            {
+                if (Time.time > nextReproduction)
+                {
+                    readyToReproduce = true;
+                    renderer.material = ready;
+                }
+            }
+        }
+    }
+
+    void GiveBirth()
+    {
+        nextReproduction = Time.time + reproductionPause;
+        var g = Instantiate(individual, transform.position, Quaternion.identity);
+        g.GetComponent<LifeCycle>().canOnlyMove = true;
+    }
+
+    bool DetectPartner()
+    {
+        var partners = Physics.OverlapSphere(transform.position, 0.5f);
+        foreach(var p in partners)
+        {
+            if (p.gameObject == this.gameObject)
+                continue;
+            var pLifeCycle = p.GetComponent<LifeCycle>();
+            if (pLifeCycle.readyToReproduce)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
--- SimpleGame/Assets/Movement.cs
+++ SimpleGame/Assets/Movement.cs
@@ -1,78 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Movement : MonoBehaviour 
+public class Movement : MonoBehaviour
 {
-	public float movementTime = 2;
-	public float lifeTime = 20;
-	public float matureAfter = 10;
-	public float reproductionPause = 2;
+    public float movementTime = 2;
 
-	float deathTime;
-	float startMovementTime;
-	float matureTime;
-	float nextReproduction;
+    Vector3 direction;
+    bool isMoving = false;
+    float startMovementTime;
 
-	bool isMoving = false;
-	bool isMature = false;
-	bool readyToReproduce = false;
+    void Update()
+    {
+        Move();
+    }
 
-	Vector3 direction;
-	
-	void Start () 
-	{
-		deathTime = Time.time + lifeTime;
-		matureTime = Time.time + matureAfter;
-	}
-	
-	void Update () 
-	{
-		if (Time.time > deathTime)
-			Destroy(gameObject);
+    void Move()
+    {
+        if (Time.time - startMovementTime > movementTime)
+            isMoving = false;
 
-		if (!isMature)
-			if (Time.time > matureTime)
-			{
-				isMature = true;
-				readyToReproduce = true;
-				GetComponent<MeshRenderer>().material.color = Color.blue;
-			}
-		
-		//if (readyToReproduce ) 
+        if (isMoving)
+        {
+            var nextPos = transform.position + (direction * Time.deltaTime);
+            if (CheckFieldBorders(nextPos))
+                isMoving = false;
+            else
+                transform.Translate(direction * Time.deltaTime);
+        }
+        else
+        {
+            var dir = Random.insideUnitCircle;
+            dir.Normalize();
+            direction = new Vector3(dir.x, 0, dir.y);
+            startMovementTime = Time.time;
+            isMoving = true;
+            movementTime = 2 + Random.Range(-0.5f, 0.5f);
+        }
+    }
 
-		Move(); 
-	}
-
-	void Move()
-	{
-		if (Time.time - startMovementTime > movementTime)
-					isMoving = false;
-
-		if (isMoving)
-		{
-			var nextPos = transform.position + (direction * Time.deltaTime);
-			if (CheckFieldBorders(nextPos))
-				isMoving = false;
-			else
-				transform.Translate(direction * Time.deltaTime);
-		}
-		else
-		{
-			var dir = Random.insideUnitCircle;
-			dir.Normalize();
-			direction = new Vector3(dir.x, 0, dir.y);
-			direction.y = 0;
-			startMovementTime = Time.time;
-			isMoving = true;
-			movementTime = 2 + Random.RandomRange(-0.5f, 0.5f);
-		}
-	}
-
-	bool CheckFieldBorders(Vector3 nextPos)
-	{
-		if (Mathf.Abs(nextPos.x) > 20f || Mathf.Abs(nextPos.z) > 20f)
-			return true;
-		return false;
-	}
+    bool CheckFieldBorders(Vector3 nextPos)
+    {
+        if (Mathf.Abs(nextPos.x) > 20f || Mathf.Abs(nextPos.z) > 20f)
+            return true;
+        return false;
+    }
 }
